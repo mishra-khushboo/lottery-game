1efae2f70393f9af173fb69e0785df9bd9cebb46
mishra-khushboo <khush27093@gmail.com>
2026-01-31 16:14:15 +0530

    improving component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
 import Lottery from './Lottery'
+import Ticket from './Ticket'
 
 function App() {
  
 
   return (
     <>
-      <Lottery/>
+      <Ticket ticket={[0, 5, 8]} />
+      <Ticket ticket={[1,8,5,4]} />
+      <Ticket ticket={[9,8,5,6,8]} />
+     
     </>
   )
 }
